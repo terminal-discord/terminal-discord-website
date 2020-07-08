4a99758180e919e0b63e68a0ac0e689615969865
Noskcaj <kcajdev@gmail.com>
2020-07-08 11:46:28 -0400

    Use query parameters

--- api/latest-build.ts
+++ api/latest-build.ts
@@ -3,8 +3,23 @@ import fetch from "node-fetch";
 
 export default async (req: NowRequest, res: NowResponse) => {
     res.setHeader("Cache-Control", "no-cache");
+    let repo = req.query["repo"];
+    let workflow_id = req.query["workflow"];
+
+    if (repo == null || repo == "") {
+        res.statusCode = 400;
+        res.json({ error: "no repo slug provided" });
+        return;
+    }
+
+    if (workflow_id == null || workflow_id == "") {
+        res.statusCode = 400;
+        res.json({ error: "no workflow id provided" });
+        return;
+    }
+
     let runs = await fetch(
-        "https://api.github.com/repos/terminal-discord/weechat-discord/actions/workflows/1329556/runs"
+        `https://api.github.com/repos/terminal-discord/${repo}/actions/workflows/${workflow_id}/runs`
     ).then((resp) => resp.json());
 
     let url = runs["workflow_runs"][0]["html_url"];
