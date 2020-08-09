f70fb4407bc5a7d387f4f1e25877e83adaa59f4f
Noskcaj <kcajdev@gmail.com>
2020-08-09 15:25:03 -0400

    Add `branch` param to filter by branch

--- api/latest-build.ts
+++ api/latest-build.ts
@@ -1,26 +1,42 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import fetch from "node-fetch";
 
+function isSet(value) {
+    return value != null && value != "";
+}
+function isUnset(value) {
+    return !isSet(value);
+}
+
 export default async (req: NowRequest, res: NowResponse) => {
     res.setHeader("Cache-Control", "no-cache");
     let repo = req.query["repo"];
     let workflow_id = req.query["workflow"];
+    let branch = req.query["branch"];
 
-    if (repo == null || repo == "") {
+    if (isUnset(repo)) {
         res.statusCode = 400;
         res.json({ error: "no repo slug provided" });
         return;
     }
 
-    if (workflow_id == null || workflow_id == "") {
+    if (isUnset(workflow_id)) {
         res.statusCode = 400;
         res.json({ error: "no workflow id provided" });
         return;
     }
 
-    let runs = await fetch(
-        `https://api.github.com/repos/terminal-discord/${repo}/actions/workflows/${workflow_id}/runs`
-    ).then((resp) => resp.json());
+    let api_url = `https://api.github.com/repos/terminal-discord/${repo}/actions/workflows/${workflow_id}/runs`;
+    let query_params = {};
+    if (isSet(branch)) {
+        query_params["branch"] = branch;
+    }
+    console.log(query_params);
+    if (new URLSearchParams(query_params).toString() != "") {
+        api_url += "?" + new URLSearchParams(query_params);
+    }
+
+    let runs = await fetch(api_url).then((resp) => resp.json());
 
     let url = runs["workflow_runs"][0]["html_url"];
 
